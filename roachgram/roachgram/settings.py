40f3d132210edc19c9b50f1f778d5950af1de816
unknown <rouchashoori@gmail.com>
2024-02-22 15:51:26 +0330

    added crispy form and some settings for login and logout

--- roachgram/roachgram/settings.py
+++ roachgram/roachgram/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users.apps.UsersConfig'
+    'users.apps.UsersConfig',
+    "crispy_forms",
+    "crispy_tailwind"
 ]
 
 MIDDLEWARE = [
@@ -126,3 +128,11 @@ MEDIA_URL = "/uploads/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "tailwind"
+
+CRISPY_TEMPLATE_PACK = "tailwind"
+
+LOGIN_REDIRECT_URL = "register-user"
+LOGOUT_REDIRECT_URL = 'login-user'
