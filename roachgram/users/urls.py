ad22f8d1f307e993d133ee6a4c814933ccaed303
unknown <rouchashoori@gmail.com>
2024-02-22 13:25:39 +0330

    added follow and unfollow routes

--- roachgram/users/urls.py
+++ roachgram/users/urls.py
@@ -5,5 +5,7 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path("@<str:username>" , views.userPage , name="user-page")
+    path("@<str:username>" , views.userPage , name="user-page"),
+    path("<str:username>/follow/" , views.followUser , name="follow-user"),
+    path("<str:username>/unfollow/" , views.unfollowUser , name="unfollow-user")
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
