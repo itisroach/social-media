761355f625039d75c49ecac344215aab9cdb7aeb
unknown <rouchashoori@gmail.com>
2024-02-22 15:59:13 +0330

    added login , logout , register rotues

--- roachgram/users/urls.py
+++ roachgram/users/urls.py
@@ -2,10 +2,17 @@ from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.contrib.auth import views as authViews
 
 urlpatterns = [
     path("@<str:username>" , views.userPage , name="user-page"),
     path("<str:username>/follow/" , views.followUser , name="follow-user"),
-    path("<str:username>/unfollow/" , views.unfollowUser , name="unfollow-user")
+    path("<str:username>/unfollow/" , views.unfollowUser , name="unfollow-user"),
+
+    # auth routes
+    path("register" , views.registeration , name="register-user"),
+    path("login" , authViews.LoginView.as_view(template_name="login.html" , redirect_authenticated_user=True) , name="login-user"),
+    path("logout" , authViews.LogoutView.as_view() , name="logout-user")
+
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
