8cbfaa991663afe6e0fa16f268ce83e98c7ec5ed
unknown <rouchashoori@gmail.com>
2024-02-22 22:21:23 +0330

    added home view

--- roachgram/feeds/views.py
+++ roachgram/feeds/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+from users.models import User , FollowUser
+from .models import Post
+from django.contrib.auth.decorators import login_required
+from django.db.models import Count
 # Create your views here.
+@login_required()
+def home(request):
+    feedPosts = Post.objects.filter(
+                user__in=FollowUser.objects.filter(
+                    follower=request.user
+                ).values_list('following')
+                ).order_by("-createdAt")
+    
+    context = {
+        "feedPosts": feedPosts
+    }
+
+    return render(request , "home.html" , context)
