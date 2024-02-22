08329b25a2bad3b2d7464fb8eb7ad96a7453978c
unknown <rouchashoori@gmail.com>
2024-02-22 20:54:02 +0330

    made models for like and post

--- roachgram/feeds/models.py
+++ roachgram/feeds/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
+from users.models import User
 # Create your models here.
+
+def mediaDirectory(instance , filename):
+    return f"{instance}/media/{filename}"
+
+class Post(models.Model):
+    user      = models.ForeignKey(User , on_delete=models.CASCADE , related_name="creator")
+    image     = models.ImageField(upload_to=mediaDirectory , blank=True , null=True)
+    video     = models.FileField(upload_to=mediaDirectory , blank=True , null=True)
+    caption   = models.TextField(max_length=512)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now=True)
+
+class Like(models.Model):
+    user = models.ForeignKey(User , on_delete=models.CASCADE , related_name="user")
+    post = models.ForeignKey(Post , on_delete=models.CASCADE , related_name="post")
