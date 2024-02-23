ea89ef00863ee55c18a14580ac917692fbf9007c
unknown <rouchashoori@gmail.com>
2024-02-24 00:34:49 +0330

    created a signal when user removes a post, the server removes all of its media

--- roachgram/feeds/signals.py
+++ roachgram/feeds/signals.py
@@ -9,8 +9,9 @@ from users.signals import deleteFile
 # before post gets deleted removes the media of it
 @receiver(pre_delete , sender=Post)
 def on_post_remove(sender , instance , **kwargs):
-    # check if image is exists
+    
     allMedia = instance.get_media()
+    # check if media is exists
     if (len(allMedia) > 0):
         for media in allMedia:
             # first change Media type to string and then removes first slash to path be recognized to os.remove
