0f17e4e71f2537948c380d35923166283c58e19a
unknown <rouchashoori@gmail.com>
2024-02-24 00:14:43 +0330

    changed model of post and seprate the media form post model

--- roachgram/feeds/models.py
+++ roachgram/feeds/models.py
@@ -3,15 +3,27 @@ from users.models import User
 # Create your models here.
 
 def mediaDirectory(instance , filename):
-    return f"{instance.user.username}/media/{filename}"
+    return f"{instance.post.user.username}/media/{filename}"
 
 class Post(models.Model):
     user      = models.ForeignKey(User , on_delete=models.CASCADE , related_name="creator")
-    image     = models.ImageField(upload_to=mediaDirectory , blank=True , null=True)
-    video     = models.FileField(upload_to=mediaDirectory , blank=True , null=True)
     caption   = models.TextField(max_length=512)
     createdAt = models.DateTimeField(auto_now_add=True)
     updatedAt = models.DateTimeField(auto_now=True)
+
+
+    def get_media(self):
+        return Media.objects.filter(post = self.id)
+
+
+
+class Media(models.Model):
+    post  = models.ForeignKey(Post , on_delete=models.CASCADE , related_name="post_media")
+    file = models.FileField(upload_to=mediaDirectory , blank=True , null=True)
+
+
+    def __str__(self) -> str:
+        return f"{self.file.url}"
 
 class Like(models.Model):
     user = models.ForeignKey(User , on_delete=models.CASCADE , related_name="user")
