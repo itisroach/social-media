2347983ed5a165e304ce671a323a0aea9f0a5a15
unknown <rouchashoori@gmail.com>
2024-02-24 00:14:11 +0330

    added signals to feeds app

--- roachgram/feeds/apps.py
+++ roachgram/feeds/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class FeedsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'feeds'
+
+
+    def ready(self) -> None:
+        from . import signals
