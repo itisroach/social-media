e79da2d3dfc34faa3d6385ed72dab316effae537
unknown <rouchashoori@gmail.com>
2024-02-24 15:46:18 +0330

    added a method to post model for getting like count of post and a method for getting type of media in template

--- roachgram/feeds/models.py
+++ roachgram/feeds/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from users.models import User
+import os
 # Create your models here.
 
 def mediaDirectory(instance , filename):
@@ -12,8 +13,18 @@ class Post(models.Model):
     updatedAt = models.DateTimeField(auto_now=True)
 
 
+    def __str__(self):
+        return f"posted by {self.user.username}"
+
     def get_media(self):
         return Media.objects.filter(post = self.id)
+    
+    def get_like_count(self):
+        return Like.objects.filter(post = self).count()
+
+    
+
+    
 
 
 
@@ -25,6 +36,23 @@ class Media(models.Model):
     def __str__(self) -> str:
         return f"{self.file.url}"
 
+
+    
+
+    def get_type(self):
+        videoFormats = [".mp4" , ".mov" , ".avi" , ".wmv" , ".avchd" , ".flv" , ".webm"]
+        imageFormats = [".jpg" , ".jpeg" , ".png" , ".gif" , ".tiff" , ".webp"]
+        name , extension = os. path.splitext(self.file.url)
+        if extension in videoFormats:
+            return "video"
+        elif extension in imageFormats:
+            return "img"
+
+
 class Like(models.Model):
     user = models.ForeignKey(User , on_delete=models.CASCADE , related_name="user")
     post = models.ForeignKey(Post , on_delete=models.CASCADE , related_name="post")
+
+
+    def __str__(self) -> str:
+        return f"{self.user.username} liked {self.post.id}"
