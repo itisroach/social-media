fc243119b9f4c407972630b4dc38d0ec4ba76e46
unknown <rouchashoori@gmail.com>
2024-02-24 23:17:09 +0330

    added bookmark model to admin page

--- roachgram/feeds/admin.py
+++ roachgram/feeds/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Post , Like , Media
+from .models import Post , Like , Media , Bookmark
 # Register your models here.
 admin.site.register(Post)
 admin.site.register(Like)
 admin.site.register(Media)
+admin.site.register(Bookmark)
