490fd8a2e20cb733efd3c99946e1f34ecf66c793
unknown <rouchashoori@gmail.com>
2024-02-24 16:47:25 +0330

    added bookmark url to urls.py

--- roachgram/feeds/urls.py
+++ roachgram/feeds/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("" , views.home , name="home-page"),
     path("post/" , views.post , name="post"),
-    path("<int:pk>/like" , views.likePost , name="like-post")
+    path("<int:pk>/like" , views.likePost , name="like-post"),
+    path("<int:pk>/bookmark" , views.bookmarkPost , name="bookmark-post")
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
