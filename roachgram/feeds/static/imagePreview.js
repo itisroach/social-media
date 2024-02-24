94cc7a580ebcfc573e9d5a5208128c972c370ff6
unknown <rouchashoori@gmail.com>
2024-02-24 15:43:15 +0330

    showing selected media to user in a proper style

--- roachgram/feeds/static/imagePreview.js
+++ roachgram/feeds/static/imagePreview.js
@@ -1,41 +1,34 @@
-
-                
-
 const preview = (e) => {
-    // const [file] = imgInp.files
-    // if (file) {
-    //     blah.src = URL.createObjectURL(file)
-    // }
-    const files =  Array.from(e.files)
+    const files = Array.from(e.files)
+  
     files.forEach(file => {
+
       const blob = URL.createObjectURL(file)
-      if (file.type.includes("image")){ 
-        
-        
+      if (file.type.includes("image")){         
         document.querySelector(".parent-for-preview").innerHTML += `
-                <div class="flex m-1 flex-wrap cursor-pointer">
-                    <div class="w-full">
-                        <img
+                      <div class="flex relative w-auto h-auto m-0.5 flex-wrap cursor-pointer">
+                        <div class="w-full w-auto h-auto">
+                          <img
                             alt="gallery"
-                            class="block h-44 w-44 rounded-lg object-cover object-center"
+                            class="h-full w-auto h-auto rounded-lg object-cover object-center"
                             src="${blob}" />
-                    </div>             
-                </div>
+                        </div>             
+                      </div>
 
         `
       }
       else if(file.type.includes("video")){
         document.querySelector(".parent-for-preview").innerHTML += `
-                <div class="flex m-1 flex-wrap cursor-pointer">
-                    <div class="w-full">
+                      <div class="flex relative w-auto h-auto m-0.5 flex-wrap cursor-pointer">
+                      <div class="w-full w-auto h-auto">
                         <video
-                            controls
-                            alt="gallery"
-                            class="block h-44 w-44 rounded-lg object-cover object-center"
-                            src="${blob}">
-                        </video>
-                    </div>             
-                </div>
+                          controls
+                          alt="gallery"
+                          class="h-full w-auto h-auto rounded-lg object-cover object-center"
+                          src="${blob}">
+                          </video>
+                      </div>             
+                    </div>
         `
       }
     })
