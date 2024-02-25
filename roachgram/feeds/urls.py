a353f57fb4f09d7a285929dddc91e6f3f08de550
unknown <rouchashoori@gmail.com>
2024-02-25 18:21:59 +0330

    added comment url and delete url and single post url

--- roachgram/feeds/urls.py
+++ roachgram/feeds/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     path("<int:pk>/like" , views.likePost , name="like-post"),
     path("<int:pk>/bookmark" , views.bookmarkPost , name="bookmark-post"),
     path("mybookmarks/" , views.GetAllBookmarks.as_view() , name="bookmark-page"),
-    path("posts/<int:pk>/delete/" , views.deletePost , name="delete-post")
+    path("posts/<int:pk>/" , views.singlePost , name="single-post-page"),
+    path("posts/<int:pk>/delete/" , views.deletePost , name="delete-post"),
+    path("posts/<int:pk>/comment" , views.comment , name="comment-post")
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
