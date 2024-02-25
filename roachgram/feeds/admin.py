91322242d60e18a52cb85cf03c0b1d42a728401f
unknown <rouchashoori@gmail.com>
2024-02-25 18:19:29 +0330

    added comment model to admin page

--- roachgram/feeds/admin.py
+++ roachgram/feeds/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Post , Like , Media , Bookmark
+from .models import Post , Like , Media , Bookmark , Comment
 # Register your models here.
 admin.site.register(Post)
 admin.site.register(Like)
 admin.site.register(Media)
 admin.site.register(Bookmark)
+admin.site.register(Comment)
