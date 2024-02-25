736e12384247cb49a8863bff13ae31a784ec9d45
unknown <rouchashoori@gmail.com>
2024-02-25 22:43:33 +0330

    added users suggestion tag to suggest users to follow

--- roachgram/feeds/templatetags/customTemplateTags.py
+++ roachgram/feeds/templatetags/customTemplateTags.py
@@ -24,3 +24,11 @@ upto.is_safe = True
 @register.simple_tag
 def followedTheUser(follower , following):
     return True if FollowUser.objects.filter(follower=follower , following=following) else False
+
+
+@register.simple_tag
+def userSuggestion(user):
+    likedByUser = Like.objects.filter(user=user)[0]
+    otherUsers = Like.objects.filter(user__in=Like.objects.filter(post=likedByUser.post).values_list("user").exclude(user=user))[:2]
+
+    return otherUsers
