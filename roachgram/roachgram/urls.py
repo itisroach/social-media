56f70a92287133441106d48a1bd254feca7d9445
unknown <rouchashoori@gmail.com>
2024-02-28 23:03:02 +0330

    added debug tool bar in url

--- roachgram/roachgram/urls.py
+++ roachgram/roachgram/urls.py
@@ -22,5 +22,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("users/" , include("users.urls")),
-    path("" , include("feeds.urls"))
+    path("" , include("feeds.urls")),
+    path("__debug__/", include("debug_toolbar.urls"))
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
