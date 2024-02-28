115156f8a2165acce3d86d43c1b267291097d4e1
unknown <rouchashoori@gmail.com>
2024-02-28 22:54:58 +0330

    not important changes

--- roachgram/feeds/static/imagePreview.js
+++ roachgram/feeds/static/imagePreview.js
@@ -6,26 +6,23 @@ const preview = (e) => {
       const blob = URL.createObjectURL(file)
       if (file.type.includes("image")){         
         document.querySelector(".parent-for-preview").innerHTML += `
-                      <div class="flex relative w-auto h-auto m-0.5 flex-wrap cursor-pointer">
-                        <div class="w-full w-auto h-auto">
-                          <img
-                            alt="gallery"
-                            class="h-full w-auto h-auto rounded-lg object-cover object-center"
-                            src="${blob}" />
-                        </div>             
-                      </div>
+                        <div class="flex relative w-1/2 h-full m-0.5 flex-wrap cursor-pointer">
+                          <div class="w-full h-full">
+                              <img
+                                alt="gallery"
+                                class="aspect-square rounded-lg object-cover object-center"
+                                src="${blob}" />
+                          </div>             
+                        </div>
 
         `
       }
       else if(file.type.includes("video")){
         document.querySelector(".parent-for-preview").innerHTML += `
-                      <div class="flex relative w-auto h-auto m-0.5 flex-wrap cursor-pointer">
-                      <div class="w-full w-auto h-auto">
-                        <video
-                          controls
-                          alt="gallery"
-                          class="h-full w-auto h-auto rounded-lg object-cover object-center"
-                          src="${blob}">
+                    <div class="flex relative w-1/2 h-full m-0.5 flex-wrap cursor-pointer">
+                      <div class="w-full h-full">
+                          <video loop class="h-full w-auto h-auto rounded-lg object-cover object-center" controls>
+                            <source src="${blob}" />
                           </video>
                       </div>             
                     </div>
