b14add76ed858ab7c4f1ad2e88b0e2b6697bf5f2
unknown <rouchashoori@gmail.com>
2024-02-28 23:01:25 +0330

    added search url

--- roachgram/feeds/urls.py
+++ roachgram/feeds/urls.py
@@ -3,6 +3,7 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path("" , views.home , name="home-page"),
     path("post/" , views.post , name="post"),
@@ -11,6 +12,7 @@ urlpatterns = [
     path("mybookmarks/" , views.GetAllBookmarks.as_view() , name="bookmark-page"),
     path("posts/<int:pk>/" , views.singlePost , name="single-post-page"),
     path("posts/<int:pk>/delete/" , views.deletePost , name="delete-post"),
-    path("posts/<int:pk>/comment" , views.comment , name="comment-post")
+    path("posts/<int:pk>/comment" , views.comment , name="comment-post"),
+    path("search/" , views.search , name="search-page")
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
