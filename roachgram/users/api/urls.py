1a23d8b2bd649517a2e5f8343d5532c43cf1b8ef
unknown <rouchashoori@gmail.com>
2024-03-01 00:18:40 +0330

    added urls for change password, update users, token , refresh token and registeration

--- roachgram/users/api/urls.py
+++ roachgram/users/api/urls.py
@@ -1,6 +1,16 @@
 from django.urls import path
 from . import viewsets
 
+
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+)
+
 urlpatterns = [
-    path("register" , viewsets.createUser , name="api-create-user")
+    path("register" , viewsets.createUser , name="api-create-user"),    
+    path("edit-user" , viewsets.UpdateUser.as_view() , name="api-update-user"),
+    path("change-password" , viewsets.ChangePassword.as_view() , name="api-change-password"),
+    # JWT authentication
+    path('token/', viewsets.MyToken.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
