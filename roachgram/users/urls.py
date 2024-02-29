795aaf65285021ea95d820a1895be8ea0b28d549
unknown <rouchashoori@gmail.com>
2024-02-29 20:13:29 +0330

    added password reset and password change urls

--- roachgram/users/urls.py
+++ roachgram/users/urls.py
@@ -13,5 +13,14 @@ urlpatterns = [
     path("logout" , authViews.LogoutView.as_view() , name="logout-user"),
 
     # profile route
-    path("profile/",  views.updateUser , name="update-user")
+    path("profile/",  views.updateUser , name="update-user"),
+
+    # change password route
+    path("change-password" , views.changesPassword , name="change-password"),
+
+    # password reset
+    path("password-reset-done" , authViews.PasswordResetDoneView.as_view(template_name="password_reset_done.html") , name="password-reset-done"),
+    path("password-reset" , views.ResetPassword.as_view() , name="password-reset"),
+    path("password-reset-confirm/<uidb64>/<token>/" , authViews.PasswordResetConfirmView.as_view(template_name="password_reset_confirm.html") , name="password_reset_confirm"),
+    path("password-reset-complete" , authViews.PasswordResetCompleteView.as_view(template_name="password_reset_complete.html") , name="password_reset_complete")
 ]
