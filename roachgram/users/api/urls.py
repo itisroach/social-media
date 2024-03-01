e00ea8987b1aa5345ac279e051450a83090a2e4e
unknown <rouchashoori@gmail.com>
2024-03-01 14:33:33 +0330

    added get one and all users urls

--- roachgram/users/api/urls.py
+++ roachgram/users/api/urls.py
@@ -10,7 +10,10 @@ urlpatterns = [
     path("register" , viewsets.createUser , name="api-create-user"),    
     path("edit-user" , viewsets.UpdateUser.as_view() , name="api-update-user"),
     path("change-password" , viewsets.ChangePassword.as_view() , name="api-change-password"),
+    path("users" , viewsets.AllUsers.as_view() , name="all-users"),
+    path("users/<str:username>" , viewsets.OneUser.as_view() , name="one-user"),
     # JWT authentication
     path('token/', viewsets.MyToken.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
 ]
