d4fee946aeac9faac0308eeb3fb779fb6b5ec219
unknown <rouchashoori@gmail.com>
2024-03-01 16:58:24 +0330

    cleanup some code and created functionalities for deleting account

--- roachgram/users/api/viewsets.py
+++ roachgram/users/api/viewsets.py
@@ -1,12 +1,18 @@
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view , parser_classes
-from .serializers import CreateUserSerializer , UpdateUserSeralizer , ChangePasswordSerializer , UserSerializer
+from .serializers import (
+                        CreateUserSerializer , 
+                        UpdateUserSeralizer , 
+                        ChangePasswordSerializer ,
+                        UserSerializer,
+                        FollowSerializer
+                        )
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
-from ..models import User
-from rest_framework.parsers import MultiPartParser
-from rest_framework.generics import UpdateAPIView , ListAPIView , RetrieveAPIView
+from ..models import User , FollowUser
+from rest_framework.generics import UpdateAPIView , ListAPIView , RetrieveAPIView , CreateAPIView
+from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 
 class MyTokenSerializer(TokenObtainPairSerializer):
@@ -26,13 +32,7 @@ class MyToken(TokenObtainPairView):
     serializer_class = MyTokenSerializer
 
 
-@api_view(["POST"])
-def createUser(request):
-    serializer = CreateUserSerializer(data=request.data)
-    serializer.is_valid(raise_exception=True)
-    serializer.create(request.data)
-    
-    return Response(serializer.data)
+
 
 
 class AllUsers(ListAPIView):
@@ -45,14 +45,37 @@ class OneUser(RetrieveAPIView):
     queryset = User.objects.all()
     lookup_field = "username"
     
+class UserView(APIView):
 
+    
+
+    def post(self, request , format=None):
+        serializer = CreateUserSerializer(data=request.data)
+         
+        if serializer.is_valid():
+            serializer.create(request.data)
+            return Response(serializer.data , status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors , status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self , request , format=None):
+        self.check_object_permissions(request , request.user)
+        
+        try:
+            user = User.objects.get(id=request.user.id)
+        except User.DoesNotExist:
+            return Response({"message": "user not found"} , status=status.HTTP_404_NOT_FOUND)
+        
+        user.delete()
+
+        return Response({"message": "user deleted successfully"}, status=status.HTTP_200_OK)
+
+    
 
 class UpdateUser(UpdateAPIView):
     serializer_class = UpdateUserSeralizer
     permission_classes = [IsAuthenticated,]
-    lookup_field = "pk"
-    
-    
+
 
     def get_queryset(self):
         return self.request.user
@@ -63,9 +86,9 @@ class UpdateUser(UpdateAPIView):
         serializer = self.get_serializer(instance , data=request.data , partial=True)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
+            return Response(serializer.data , status=status.HTTP_200_OK)
         else:
-            return Response(serializer.errors)
+            return Response(serializer.errors , status=status.HTTP_400_BAD_REQUEST)
 
 
 class ChangePassword(UpdateAPIView):
@@ -94,3 +117,35 @@ class ChangePassword(UpdateAPIView):
                     'message': 'Password updated successfully'})
           
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+
+class FollowView(APIView):
+    
+    permission_classes = [IsAuthenticated,]
+
+
+    def post(self , request , format=None):
+
+        followInstance = FollowUser.objects.filter(follower=request.user , following=request.data["following"])
+
+        if followInstance.exists():
+            return Response({"message": "the authorized user is already following this user"} , status=status.HTTP_409_CONFLICT)
+
+        request.data["follower"] = request.user.id
+        serializer = FollowSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data , status=status.HTTP_201_CREATED)
+        return Response(serializer.errors , status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self , request , format=None):
+        try:
+            followInstance = FollowUser.objects.get(follower=request.user , following=request.data["following"])
+        except FollowUser.DoesNotExist:
+            return Response({"message": "follower is not following this user"},status=status.HTTP_404_NOT_FOUND)
+
+        followInstance.delete()
+
+        return Response({"message": "unfollowed successfully"},status=status.HTTP_204_NO_CONTENT)
