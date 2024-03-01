0bf360b9351afc188c2fffaa32efa5b2c62f9614
unknown <rouchashoori@gmail.com>
2024-03-01 16:57:32 +0330

    change name of urls and updated the urls

--- roachgram/users/api/urls.py
+++ roachgram/users/api/urls.py
@@ -7,13 +7,15 @@ from rest_framework_simplejwt.views import (
 )
 
 urlpatterns = [
-    path("register" , viewsets.createUser , name="api-create-user"),    
+    path("user" , viewsets.UserView.as_view() , name="api-user"),    
     path("edit-user" , viewsets.UpdateUser.as_view() , name="api-update-user"),
     path("change-password" , viewsets.ChangePassword.as_view() , name="api-change-password"),
     path("users" , viewsets.AllUsers.as_view() , name="all-users"),
+    path("users/follow" , viewsets.FollowView.as_view() , name="follow-user"),
     path("users/<str:username>" , viewsets.OneUser.as_view() , name="one-user"),
+    
     # JWT authentication
-    path('token/', viewsets.MyToken.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('login', viewsets.MyToken.as_view(), name='token_obtain_pair'),
+    path('login/refresh', TokenRefreshView.as_view(), name='token_refresh'),
 
 ]
