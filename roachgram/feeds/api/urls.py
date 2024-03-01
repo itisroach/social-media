5e26a13a509f7c5607eb3b05ab4272ec4f64acee
unknown <rouchashoori@gmail.com>
2024-03-01 23:13:35 +0330

    added like url

--- roachgram/feeds/api/urls.py
+++ roachgram/feeds/api/urls.py
@@ -3,5 +3,7 @@ from . import viewsets
 
 urlpatterns = [
     path("" , viewsets.PostViews.as_view() ,name="api-posts"),
-    path("/<int:pk>" , viewsets.PostDetailView.as_view() , name="api-post-detail")
+    path("<int:pk>" , viewsets.PostDetailView.as_view() , name="api-post-detail"),
+    path("users/<str:username>" , viewsets.UserPostListView.as_view() , name="api-user-post"),
+    path("like" , viewsets.LikePostView.as_view() , name="api-like-post")
 ]
