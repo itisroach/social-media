b37cd9212ca1fc594aef4703cd88963a61b0c98d
unknown <rouchashoori@gmail.com>
2024-03-02 22:38:12 +0330

    removed create method in user seralizer

--- roachgram/users/api/serializers.py
+++ roachgram/users/api/serializers.py
@@ -17,14 +17,6 @@ class CreateUserSerializer(serializers.ModelSerializer):
         fields = ["name" , "username" , "about" , "password" , "email" , "profile"]
 
 
-    def create(self, validated_data):
-        password = validated_data.pop('password', None)
-        instance = self.Meta.model(**validated_data)
-        if password is not None:
-            instance.set_password(password)
-        instance.save()
-        return instance
-    
 
     
 class UpdateUserSeralizer(serializers.ModelSerializer):
