dc978f0032adf90c6fdc693cabe52d5c9d526bfb
unknown <rouchashoori@gmail.com>
2024-03-02 20:01:24 +0330

    removed upload media url

--- roachgram/feeds/api/urls.py
+++ roachgram/feeds/api/urls.py
@@ -3,7 +3,6 @@ from . import viewsets
 
 urlpatterns = [
     path("" , viewsets.PostViews.as_view() ,name="api-posts"),
-    path("upload/media" , viewsets.SaveMedia.as_view() , name="api-upload-media-post"),
     path("<int:pk>" , viewsets.PostDetailView.as_view() , name="api-post-detail"),
     path("users/<str:username>" , viewsets.UserPostListView.as_view() , name="api-user-post"),
     path("like" , viewsets.LikePostView.as_view() , name="api-like-post")
