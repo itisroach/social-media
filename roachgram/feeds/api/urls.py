a7ffe9ee8633d303c51ca9b93f0d1a1b15f0a9b3
unknown <rouchashoori@gmail.com>
2024-03-07 15:35:30 +0330

    added comment url

--- roachgram/feeds/api/urls.py
+++ roachgram/feeds/api/urls.py
@@ -4,6 +4,7 @@ from . import viewsets
 urlpatterns = [
     path("" , viewsets.PostViews.as_view() ,name="api-posts"),
     path("<int:pk>" , viewsets.PostDetailView.as_view() , name="api-post-detail"),
+    path("<int:pk>/comments" , viewsets.CommentView.as_view() , name="api-comment"),
     path("users/<str:username>" , viewsets.UserPostListView.as_view() , name="api-user-post"),
     path("like" , viewsets.LikePostView.as_view() , name="api-like-post")
 ]
