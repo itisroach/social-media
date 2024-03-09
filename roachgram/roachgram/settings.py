8480dfbcef65915e8eadd056b7fb7372c1d9a88b
unknown <rouchashoori@gmail.com>
2024-03-09 14:00:35 +0330

    some pycache and DB changes

--- roachgram/roachgram/settings.py
+++ roachgram/roachgram/settings.py
@@ -71,7 +71,9 @@ REST_FRAMEWORK = {
 
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.CursorPagination',
+    'PAGE_SIZE': 10
 }
 
 
