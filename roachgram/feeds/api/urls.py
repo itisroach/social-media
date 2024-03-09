c9d8fa02fdbe18827c50560bc90876eb308ed31d
unknown <rouchashoori@gmail.com>
2024-03-09 19:59:19 +0330

    added bookmark url

--- roachgram/feeds/api/urls.py
+++ roachgram/feeds/api/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("<int:pk>" , viewsets.PostDetailView.as_view() , name="api-post-detail"),
     path("<int:pk>/comments" , viewsets.CommentView.as_view() , name="api-comment"),
     path("users/<str:username>" , viewsets.UserPostListView.as_view() , name="api-user-post"),
-    path("like" , viewsets.LikePostView.as_view() , name="api-like-post")
+    path("like" , viewsets.LikePostView.as_view() , name="api-like-post"),
+    path("bookmark" , viewsets.BoookmarkView.as_view() , name="api-bookmark-post")
 ]
