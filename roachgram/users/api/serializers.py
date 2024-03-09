5adcf7fbccd6f7609e9589a4cd20769105c4df18
unknown <rouchashoori@gmail.com>
2024-03-09 14:01:46 +0330

    created isFollowing field for user knowing if a users follows the fetched user

--- roachgram/users/api/serializers.py
+++ roachgram/users/api/serializers.py
@@ -4,11 +4,22 @@ from rest_framework.decorators import parser_classes
 from rest_framework.parsers import MultiPartParser , FormParser , JSONParser , FileUploadParser
 
 class UserSerializer(serializers.ModelSerializer):
-
+    isFollowing = serializers.SerializerMethodField(method_name="is_following_user")
 
     class Meta:
         model = User
-        fields = ["id" , "name" , "username" , "about" , "profile" , "date_joined"]
+        fields = ["id" , "name" , "username" , "isFollowing" , "about" , "profile" , "date_joined"]
+
+    def is_following_user(self , obj):
+        isFollowing = False
+        request = self.context.get("request")
+        try:
+            FollowUser.objects.get(follower=request.user.id)
+            isFollowing = True
+        except FollowUser.DoesNotExist:
+            pass
+
+        return isFollowing
     
 
 class CreateUserSerializer(serializers.ModelSerializer):
