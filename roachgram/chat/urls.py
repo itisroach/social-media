eb1f2780433ffb954f1bf9e85c23a3bebde1bdb9
unknown <rouchashoori@gmail.com>
2024-05-03 23:33:14 +0330

    Created url for getting single room

--- roachgram/chat/urls.py
+++ roachgram/chat/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("" , views.chat , name="chat-page")
+    path("" , views.chat , name="chat-page"),
+    path("<str:room_name>" , views.chatRoom , name="chat-room")
 ]
