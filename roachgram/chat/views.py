65b8971abe6df681f95fe05f2d534f5bcb434687
unknown <rouchashoori@gmail.com>
2024-05-03 21:58:09 +0330

    rendered users who logged in user can message

--- roachgram/chat/views.py
+++ roachgram/chat/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from users.models import FollowUser
 
-# Create your views here.
-
-
+@login_required
 def chat(request):
-    return render(request , "chatPage.html")
+    followedUsers = FollowUser.objects.filter(follower=request.user)
+    
+    context = {
+        "available_users_to_chat": followedUsers
+    }
+
+    return render(request , "chatPage.html" , context)
