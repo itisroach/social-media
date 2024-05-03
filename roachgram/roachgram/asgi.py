ae85e26e376c7c5bb25fe732c9be38423fe3dbd1
unknown <rouchashoori@gmail.com>
2024-05-04 01:54:39 +0330

    created routes for ws protocol

--- roachgram/roachgram/asgi.py
+++ roachgram/roachgram/asgi.py
@@ -9,15 +9,21 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 
 import os
 
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter , URLRouter
 from django.core.asgi import get_asgi_application
-
+from channels.auth import AuthMiddlewareStack
+from chat import routing
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'roachgram.settings')
 
 django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            routing.websocket_urlpatterns
+        )
+    )
 })
 
 
