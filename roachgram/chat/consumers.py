2b4f75ca685114237093be533c0c169d6f94490b
unknown <rouchashoori@gmail.com>
2024-05-04 00:23:17 +0330

    creaetd logic for sending message

---
+++ roachgram/chat/consumers.py
@@ -0,0 +1,54 @@
+import json
+from channels.generic.websocket import AsyncWebsocketConsumer
+from .models import Room , Message
+from users.models import User
+from channels.db import database_sync_to_async
+
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        room_name = self.scope["url_route"]["kwargs"]["room_name"]
+
+
+        self.roomName = room_name
+        await self.channel_layer.group_add(
+            self.roomName,
+            self.channel_name
+        )
+
+        await self.accept()
+
+    async def disconnect(self, code):
+        await self.channel_layer.group_discard(
+            self.roomName,
+            self.channel_name
+        )
+    
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+        message = data["message"]
+        username = data["sender"]
+
+        await self.channel_layer.group_send(
+            self.roomName,
+            {
+                'type': "sendMessage",
+                "message": message,
+                "username": username
+            }
+        )
+    
+    async def sendMessage(self , event):
+        await self.createMessage(event)
+        response = {
+            "sender": event["username"],
+            "message": event["message"]
+        }
+
+        await self.send(json.dumps({"data": response}))
+
+    @database_sync_to_async
+    def createMessage(self , data):
+        room = Room.objects.get(name=self.roomName)
+        sender = User.objects.get(username=data["username"])
+        new_message = Message(room=room , message=data["message"] , sender=sender)
+        new_message.save()
