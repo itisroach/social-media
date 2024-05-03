1a68d7bf65b68065c8f9ced427bc433d5e5b4c86
unknown <rouchashoori@gmail.com>
2024-05-03 21:57:15 +0330

    created DB for chat app

--- roachgram/chat/models.py
+++ roachgram/chat/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+class Room(models.Model):
+    name = models.CharField(max_length=256)
+
+    def __str__(self) -> str:
+        return self.name
+    
+class Message(models.Model):
+    room = models.ForeignKey(Room , on_delete=models.CASCADE)
+    message = models.TextField()
+    sender = models.ForeignKey(User , on_delete=models.CASCADE)
+
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return f'{self.sender.username} sent to {self.room.name}'
