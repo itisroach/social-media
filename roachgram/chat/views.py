7abbefe49cc9caa23527e3e7e77a83ca8c2e839e
unknown <rouchashoori@gmail.com>
2024-05-03 23:33:46 +0330

    created logic for creating a room

--- roachgram/chat/views.py
+++ roachgram/chat/views.py
@@ -1,9 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from django.contrib.auth.decorators import login_required
 from users.models import FollowUser
+from .models import Room
+from users.models import User
+from django.http import Http404
 
 @login_required
 def chat(request):
+
+    if request.method == "POST":
+        reciverId = request.POST.get("receiver")
+        return redirect("chat-room" , f"{request.user.id}-{reciverId}")
+
     followedUsers = FollowUser.objects.filter(follower=request.user)
     
     context = {
@@ -11,3 +19,25 @@ def chat(request):
     }
 
     return render(request , "chatPage.html" , context)
+
+
+@login_required
+def chatRoom(request , room_name):
+    try:
+        room = Room.objects.get(name=room_name)
+    except Room.DoesNotExist:
+        room = Room(name=room_name)
+        room.save()
+
+    try:
+        # room name is created by sender's id (logged in user) and receiver's id and like this sender-receiver
+        receiverId = room.name.split("-")[1]
+        receiver = User.objects.get(id=receiverId)
+    except User.DoesNotExist:
+        raise Http404
+
+    context = {
+        "room_name": room.name,
+        "receiver": receiver
+    }
+    return render(request , "chatRoom.html" , context)
