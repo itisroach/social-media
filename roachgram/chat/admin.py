8cc8464b119e8a1fbf2ea98acad732364615e263
unknown <rouchashoori@gmail.com>
2024-05-04 21:01:45 +0330

    added connection history table to admin page

--- roachgram/chat/admin.py
+++ roachgram/chat/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Room , Message
+from .models import Room , Message , ConnectionHistory
 
 admin.site.register(Room)
+admin.site.register(ConnectionHistory)
 admin.site.register(Message)
