1e5f758026faf5e9cd3a5bdef75fbbc3b3243e9e
unknown <rouchashoori@gmail.com>
2024-05-04 13:49:47 +0330

    fixed some bugs

--- roachgram/users/urls.py
+++ roachgram/users/urls.py
@@ -8,16 +8,6 @@ urlpatterns = [
     path("login" , authViews.LoginView.as_view(template_name="login.html" , redirect_authenticated_user=True) , name="login-user"),
     path("logout" , authViews.LogoutView.as_view() , name="logout-user"),
 
-    path("<str:username>/" , views.userPage , name="user-page"),
-    path("<str:username>/likes" , views.userPageLikes , name="user-page-likes"),
-    path("<str:username>/replies" , views.userPageReplies , name="user-page-replies"),
-    path("<str:username>/follow/" , views.followUser , name="follow-user"),
-    path("<str:username>/unfollow/" , views.unfollowUser , name="unfollow-user"),
-    path("<str:username>/followers" , views.userFollowers , name="user-followers-page"),
-    path("<str:username>/followings" , views.userFollowings , name="user-followings-page"),
-    
-    
-
     # profile route
     path("profile/",  views.updateUser , name="update-user"),
 
@@ -28,5 +18,16 @@ urlpatterns = [
     path("password-reset-done" , authViews.PasswordResetDoneView.as_view(template_name="password_reset_done.html") , name="password-reset-done"),
     path("password-reset" , views.ResetPassword.as_view() , name="password-reset"),
     path("password-reset-confirm/<uidb64>/<token>/" , authViews.PasswordResetConfirmView.as_view(template_name="password_reset_confirm.html") , name="password_reset_confirm"),
-    path("password-reset-complete" , authViews.PasswordResetCompleteView.as_view(template_name="password_reset_complete.html") , name="password_reset_complete")
+    path("password-reset-complete" , authViews.PasswordResetCompleteView.as_view(template_name="password_reset_complete.html") , name="password_reset_complete"),
+
+    path("<str:username>/" , views.userPage , name="user-page"),
+    path("<str:username>/likes" , views.userPageLikes , name="user-page-likes"),
+    path("<str:username>/replies" , views.userPageReplies , name="user-page-replies"),
+    path("<str:username>/follow/" , views.followUser , name="follow-user"),
+    path("<str:username>/unfollow/" , views.unfollowUser , name="unfollow-user"),
+    path("<str:username>/followers" , views.userFollowers , name="user-followers-page"),
+    path("<str:username>/followings" , views.userFollowings , name="user-followings-page"),
+    
+    
+    
 ]
