9c292d940b6669f7a2b55a7aead1deaf765e279c
unknown <rouchashoori@gmail.com>
2024-05-06 19:44:57 +0330

    added websocket urls for notifications

--- roachgram/users/routing.py
+++ roachgram/users/routing.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .consumers import NotificationConsumer
+from .consumers import NotificationConsumer , SeenNotificationConsumer
 
 websocket_urlpatterns = [
-    path("notification/<str:username>/" , NotificationConsumer.as_asgi())
+    path("notification/<str:username>/" , NotificationConsumer.as_asgi()),
+    path("notification/<str:username>/<int:last_notif_pk>/seen/" , SeenNotificationConsumer.as_asgi())
 ]
