c80e79171ed865a837a1690c3746ed9f148e1f22
unknown <rouchashoori@gmail.com>
2024-05-07 11:24:32 +0330

    added STATIC_ROOT variable

--- roachgram/roachgram/settings.py
+++ roachgram/roachgram/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2_ws!k#=akz$pe^2)sdv4ohpho*n7xxygaq40npx-w%_)glvn1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
@@ -228,7 +228,7 @@ AUTH_USER_MODEL = 'users.User'
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads/')
 MEDIA_URL = "/uploads/"
 
